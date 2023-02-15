a135a0edb87ea760bfd9ef5656276fd23d370c5d
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-02-14 22:26:12 -0300

    movendo para model

--- src/main/java/br/upe/ppsw/jabberpoint/apresentacao/model/XMLAccessor.java
+++ src/main/java/br/upe/ppsw/jabberpoint/apresentacao/model/XMLAccessor.java
@@ -1,4 +1,4 @@
-package br.upe.ppsw.jabberpoint.apresentacao;
+package br.upe.ppsw.jabberpoint.apresentacao.model;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -14,11 +14,7 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import br.upe.ppsw.jabberpoint.apresentacao.model.BitmapItem;
-import br.upe.ppsw.jabberpoint.apresentacao.model.Presentation;
-import br.upe.ppsw.jabberpoint.apresentacao.model.Slide;
-import br.upe.ppsw.jabberpoint.apresentacao.model.SlideItem;
-import br.upe.ppsw.jabberpoint.apresentacao.model.TextItem;
+import br.upe.ppsw.jabberpoint.apresentacao.Accessor;
 
 public class XMLAccessor extends Accessor {
 
